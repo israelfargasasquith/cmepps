ac5cb7d197c9f1e9aa68bf42eeb2ea45522511db
israe <israe@Raeliss.mshome.net>
2024-09-30 16:16:36 +0200

    Commit 5

--- src/tutorial/Applicacion.java
+++ src/tutorial/Applicacion.java
@@ -9,6 +9,7 @@ public class Applicacion {
 		System.out.println("Vamos ha hacer una brach");
 		System.out.println("Vamos hacer un cambio en la branch1");
 		System.out.println("Hemos unido master y la branch1");
+		System.out.println("Nueva branch 2 con commit 5 y lista de todos");
 	}
 
 }
